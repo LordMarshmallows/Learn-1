640a662896fb14951de0fa6f384e685cd4b39d74
LordMarshmallows <46316876+LordMarshmallows@users.noreply.github.com>
2022-06-26 12:28:22 +0300

    Добавление привязки

---
+++ Learn 1/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Learn_1.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void onPropertyChanged ([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T> (ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            onPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
---
+++ Learn 1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Learn_1.ViewModels.Base;
+
+namespace Learn_1.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "окошко";
+
+        /// <summary> Заголовок </summary>
+        
+        public string Title
+        {
+            get => _Title;
+
+            set => Set(ref _Title, value);
+        }
+    }
+}
